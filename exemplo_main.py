9e83595b1d7e8f2e4a5de8d870a939966080f2a3
Diego Braga <40781253+diegobraga92@users.noreply.github.com>
2022-11-05 11:43:12 -0300

    Add files via upload

---
+++ exemplo_main.py
@@ -0,0 +1,96 @@
+import sys
+print ('Running Inspector Gadget version 02/2019')
+print (sys.version)
+
+import time
+import glob
+import os
+import subprocess
+from monitor import monitor
+from os import listdir
+from os.path import isfile, join
+import shutil
+import json
+import hashlib
+
+def create_output_dir(workingdir):
+	csvdir = 'raw'
+	curdate = time.strftime("%d_%m_%Y")
+	curtime = time.strftime("%H%M%S")
+	dir = workingdir + '/' + str(curdate)
+	
+	try:
+		os.mkdir(dir)
+	except:
+		print ('already created dir ' + dir)
+
+	csvdir = dir + '/raw'
+	try:
+		os.mkdir(csvdir)
+	except:
+		print ('already created dir ' + csvdir)
+
+	prefix = str(curdate)+'_'+str(curtime)
+	return csvdir,prefix
+
+# EXPERIMENT CONFIGURATION - PRECISA MODIFICAR #
+interval = 0.1 
+samples = 60
+executionsTrain = 50
+executionsTest = 30
+input_type = 'Decision_Tree' # CRIAR MANUALMENTE
+
+# INTERNAL CONFIGURATION #
+
+xpath = os.getcwd() + "/" + input_type
+print('experiment path: ' + xpath)
+
+bpath = os.getcwd() + "/"
+print('bin path: '+ bpath)
+
+# PRECISA MODIFICAR - COLOCAR NOME DA APLICACAO #
+exefiles = glob.glob(bpath+"/dtc_experiment_code*.py")
+# #
+
+output_path,prefix = create_output_dir(xpath)
+
+experiment_tag = 'data'
+prefix = experiment_tag
+print("Configuration: "+ prefix)
+
+# PRECISA MODIFICAR - USADO PARA PASSAR COMO ARGUMENTO #
+configs = {
+	"SMALLEST":		"iris haberman_survival",
+	"SMALL":		"wine heart_disease",
+	"MEDIUM-SMALL":	"banknote pima_indians_diabetes",
+	"MEDIUM-LARGE":	"ionosphere sonar",
+	"LARGE":		"breast_cancer phoneme",
+	"LARGEST":		"oil_spill mammography"
+}
+# #
+
+print(exefiles)
+
+for sut_server in exefiles:
+	sut_server = sut_server.replace('\\','/')
+	working_path = xpath
+
+	print ('working path: ' + working_path)
+	print ('SUTs: ' + sut_server)
+	
+        # PRECISA MODIFICAR - COLOCAR NOME DA APLICACAO SEM ERROS #
+        # MUDAR TAMBEM O -22 para o tamanho do nome do arquivo #
+	if sut_server[-22:] == "dtc_experiment_code.py":
+        # #
+		executions = executionsTrain
+	else:
+		executions = executionsTest
+
+	for key, config in configs.items():
+		for i in range(executions):
+			print ("execution " + str(i))
+			m = monitor(sut_server,key,config,output_path,interval,samples,False,True)
+			m.get_samples()
+			m.dump_csv(i)
+
+print("Finished")
